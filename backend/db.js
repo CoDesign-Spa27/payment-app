f48f2fe180f659b81d416da586a6228fa1f7b31e
2sandeep7 <ssandeep8696@gmail.com>
2024-02-28 17:16:26 -0500

    added account schema in database

--- backend/db.js
+++ backend/db.js
@@ -1,10 +1,11 @@
 const mongoose=require('mongoose');
+ 
 
 mongoose.connect('mongodb+srv://mytuf8289:bElGZXrUA0lyf6yb@cluster0.pf4cvit.mongodb.net/payment-app').then((data)=>{console.log(data + " Connected to data base ");})
 
 const userSchema=new mongoose.Schema({
    username:{
-    type:'string',
+    type:String,
     required:true,
     unique:true,
     trim:true,
@@ -13,23 +14,41 @@ const userSchema=new mongoose.Schema({
     index: true
    },
    firstName:{
-    type:'string',
+    type:String,
     required:true,
     trim:true,
    },
    lastName:{
     trim:true,
-    type:'string'
+    type:String
    },
    password:{
-   type:'string',
+   type:String,
    required:true,
    min:6
    }
 })
 
-const User=mongoose.model('User',userSchema);
-module.exports={
-    User
-}
+const accountSchema=new mongoose.Schema({
 
+    userId:{
+        required:true,
+        ref:'User',
+        type:mongoose.Schema.Types.ObjectId,
+
+    },
+    balance:{
+        type:Number
+    }
+
+
+})
+
+
+const User=mongoose.model('User',userSchema);
+
+const Account=mongoose.model('Account',accountSchema)
+
+module.exports={
+    User,Account
+}
